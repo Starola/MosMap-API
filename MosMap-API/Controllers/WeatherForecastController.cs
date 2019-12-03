8c1db8e5f7f6a0557fa22820183ef7f03db04f28
Starola <christ-lu@gmx.de>
2019-12-03 15:08:38 +0100

    feature/deletePartOfExanpleCode

--- MosMap-API/Controllers/WeatherForecastController.cs
+++ MosMap-API/Controllers/WeatherForecastController.cs
@@ -30,8 +30,7 @@ namespace MosMap_API.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
                     Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
+                    TemperatureC = rng.Next(-20, 55)
                 })
                 .ToArray();
         }
--- MosMap-API/WeatherForecast.cs
+++ MosMap-API/WeatherForecast.cs
@@ -9,7 +9,5 @@ namespace MosMap_API
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
     }
 }
