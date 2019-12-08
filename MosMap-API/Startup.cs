5fe6f29f343fffadaf3889f040cb559d5294ba77
Starola <christ-lu@gmx.de>
2019-12-08 16:36:19 +0100

    feature/AddCorsSupport

--- MosMap-API/Startup.cs
+++ MosMap-API/Startup.cs
@@ -29,6 +29,7 @@ namespace MosMap_API
         {
             services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
